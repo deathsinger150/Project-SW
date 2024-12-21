ccbdd87317050236a01a6600085f97908e14db3c
Seif Kassab <121317418+deathsinger150@users.noreply.github.com>
2024-12-21 20:22:56 +0200

    Delete Testing event creation.py

--- Testing event creation.py
+++
@@ -1,9 +0,0 @@
-def test_event_creation_with_db():
-    event_manager = EventManager()
-    db = MockDatabase()
-    event_manager.set_database(db)
-
-    event_details = {"name": "Hackathon", "date": "2024-01-15"}
-    event_manager.create_event(event_details)
-
-    assert db.get_event("Hackathon") == event_details
